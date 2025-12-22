645a455df3329d11f0d456e5a8631b377593fc1c
JAG7070 <pej@crawlsoft.com>
2025-12-22 19:56:25 +0100

    Trying out Composable functions.

--- app/src/main/java/com/crawlsoft/signalclient/MainActivity.kt
+++ app/src/main/java/com/crawlsoft/signalclient/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.crawlsoft.signalclient.ui.theme.SignalClientTheme
@@ -16,24 +22,29 @@ import com.crawlsoft.signalclient.ui.theme.SignalClientTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            SignalClientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            GreetingPreview()
         }
     }
 }
 
 @Composable
+fun LoginButton() {
+    var buttonClickedCount by remember { mutableStateOf(0) }
+    Button( onClick = {
+        buttonClickedCount++
+        println("Button clicked! Count: $buttonClickedCount")
+    }) {
+        Text( text = "Click me! Count: $buttonClickedCount")
+    }
+    Greeting(" your Android")
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello again you stupid $name!",
         modifier = modifier
     )
 }
@@ -42,6 +53,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SignalClientTheme {
-        Greeting("Android")
+        Column() {
+            Greeting(" my Android")
+            LoginButton()
+        }
     }
 }
